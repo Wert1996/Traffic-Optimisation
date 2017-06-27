c20dc9316ab3841baab87fbeb74fd36c217787fb
Anshumaan Parashar <apairs.anshumaan@gmail.com>
2017-06-27 14:51:28 +0530

    Add maps, config file.

---
+++ control.py
@@ -0,0 +1,12 @@
+import os, sys
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+
+sumoBinary = "/usr/bin/sumo-gui"
+sumoCmd = [sumoBinary, "-c", 'bangalore.sumo.cfg']
+import traci
+traci.start(sumoCmd)
